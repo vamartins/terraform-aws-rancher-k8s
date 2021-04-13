5c9023bee73d39fbf82489b3dc133001b54d14ba
Vagner Almeida Martins <vagner.samm@gmail.com>
2021-04-13 13:30:10 -0300

    Arquivo para ignorar diretórios e atualização do shell para instalar o git

--- rancher.sh
+++ rancher.sh
@@ -2,3 +2,7 @@
 sudo apt-get update -y
 sudo curl https://releases.rancher.com/install-docker/19.03.sh | sh
 sudo usermod -aG docker ubuntu
+sudo apt-get install git -y
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
